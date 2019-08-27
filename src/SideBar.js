646a447c1757250e16f4a61f6335215b999a5580
aless-ishy <alessandro.ishy.medeiros@gmail.com>
2019-08-27 12:01:20 -0400

    Resolução pseudo dinâmica.

--- src/SideBar.js
+++ src/SideBar.js
@@ -27,7 +27,8 @@ class SideBar extends React.Component {
             iA: {pontuação: 0},
             posição: {i: 0, j: 0, jogador: true},
             matrizExterna: this.matrizInicialExterna(),
-            matrizInterna: this.matrizInicialInterna()
+            matrizInterna: this.matrizInicialInterna(),
+            resolução: {height: (window.innerHeight -10) + "px", width: (window.innerHeight -10) + "px"}
         };
 
         this.handleClick = this.handleClick.bind(this);
@@ -35,8 +36,10 @@ class SideBar extends React.Component {
     }
 
     matrizInicialExterna() {
-        let x = window.innerWidth * 0.5 - 450;
-        let y = window.innerHeight * 0.5 - 450;
+        let resolução = window.innerHeight -10;
+        let ppq = 254*resolução/1200;
+        let x = window.innerWidth * 0.5 - resolução/2;
+        let y = window.innerHeight * 0.5 - resolução/2;
         let A = [
             [null, null, null],
             [null, null, null],
@@ -44,8 +47,8 @@ class SideBar extends React.Component {
         ];
         for (let i = 0; i < 3; i++)
             for (let j = 0; j < 3; j++) {
-                let x_ = x + 209.5 + (j) * 190.5;
-                let y_ = y + 209.5 + (i) * 190.5;
+                let x_ = x + 346*resolução/1200 - 50 + (j) * ppq;
+                let y_ = y + 346*resolução/1200 - 50 + (i) * ppq;
                 x_ = x_ + "px";
                 y_ = y_ + "px";
                 A[i][j] = {
@@ -58,8 +61,8 @@ class SideBar extends React.Component {
             }
         for (let i = 0; i < 2; i++) {
             let j = 2;
-            let x_ = x + 209.5 + (j) * 190.5;
-            let y_ = y + 209.5 + (i) * 190.5;
+            let x_ = x + 346*resolução/1200 - 50 + (j) * ppq;
+            let y_ = y + 346*resolução/1200 - 50 + (i) * ppq;
             x_ = x_ + "px";
             y_ = y_ + "px";
             A[i][j] = {
@@ -72,8 +75,8 @@ class SideBar extends React.Component {
         }
         for (let i = 1; i < 3; i++) {
             let j = 0;
-            let x_ = x + 209.5 + (j) * 190.5;
-            let y_ = y + 209.5 + (i) * 190.5;
+            let x_ = x + 346*resolução/1200 - 50 + (j) * ppq;
+            let y_ = y + 346*resolução/1200 - 50 + (i) * ppq;
             x_ = x_ + "px";
             y_ = y_ + "px";
             A[i][j] = {
@@ -90,31 +93,20 @@ class SideBar extends React.Component {
     }
 
     matrizInicialInterna() {
-        let x = window.innerWidth * 0.5 - 450;
-        let y = window.innerHeight * 0.5 - 450;
+        let resolução = window.innerHeight -10;
+        let ppq = 254*resolução/1200;
+        let x = window.innerWidth * 0.5 - resolução/2;
+        let y = window.innerHeight * 0.5 - resolução/2;
         let B = [
             [null, null],
             [null, null]
 
         ];
-        for (let i = 0; i < 2; i++)
-            for (let j = 0; j < 2; j++) {
-                let x_ = x + 304.75 + (j) * 190.5;
-                let y_ = y + 304.75 + (i) * 190.5;
-                x_ = x_ + "px";
-                y_ = y_ + "px";
-                B[i][j] = {
-                    player: 0,
-                    posição: {
-                        left: x_, top: y_, height: "100px",
-                        width: "100px", position: "absolute"
-                    }
-                };
-            }
+
         for (let i = 0; i < 2; i++) {
             let j = 1;
-            let x_ = x + 304.75 + (j) * 190.5;
-            let y_ = y + 304.75 + (i) * 190.5;
+            let x_ = x + 473*resolução/1200-50 + (j) * ppq;
+            let y_ = y + 473*resolução/1200-50 + (i) * ppq;
             x_ = x_ + "px";
             y_ = y_ + "px";
             B[i][j] = {
@@ -125,10 +117,11 @@ class SideBar extends React.Component {
                 }
             };
         }
+
         for (let i = 0; i < 2; i++) {
             let j = 0;
-            let x_ = x + 304.75 + (j) * 190.5;
-            let y_ = y + 304.75 + (i) * 190.5;
+            let x_ = x + 473*resolução/1200-50 + (j) * ppq;
+            let y_ = y + 473*resolução/1200-50 + (i) * ppq;
             x_ = x_ + "px";
             y_ = y_ + "px";
             B[i][j] = {
@@ -158,26 +151,27 @@ class SideBar extends React.Component {
     }
 
     posicionamento(x, y) {
-
-        let x_ = x - (window.innerWidth * 0.5 - 450);
-        let y_ = y - (window.innerHeight * 0.5 - 450);
-        if (x_ > 259.5 && x_ < 640.5 && y_ > 259.5 && y_ < 640.5) {
+        let resolução = window.innerHeight -10;
+        let ppq = 254*resolução/1200;
+        let x_ = x - (window.innerWidth * 0.5 - resolução);
+        let y_ = y - (window.innerHeight * 0.5 - resolução);
+        if (x_ > (resolução/1200*346) && x_ < (resolução/1200*854) && y_ > (resolução/1200*346) && y_ < (resolução/1200*854) ) {
             //Posicão do Vértice do 1° quadrado interno
             //x = y = 346*tamanho_do_lado__da_imagem/tamanho_do_lado__da_imagem_original = 346*900/1200 = 259.5
             //Pixel por Quadrado = 254*900/1200 = 190.5
-            let a = Math.trunc((x_ - 259.5) / 190.5);
-            let b = Math.trunc((y_ - 259.5) / 190.5);
+            let a = Math.trunc((x_ - resolução/1200*346) / ppq);
+            let b = Math.trunc((y_ - resolução/1200*346) / ppq);
 
-            if ((y_ - 0.75 * (346 + b * 254)) > Math.abs(x_ - 0.75 * (473 + a * 254)) &&
-                (y_ - 0.75 * (600 + b * 254)) < -Math.abs(x_ - 0.75 * (473 + a * 254)))
+            if ((y_ - resolução/1200 * (346 + b * 254)) > Math.abs(x_ - resolução/1200 * (473 + a * 254)) &&
+                (y_ - resolução/1200 * (600 + b * 254)) < -Math.abs(x_ - resolução/1200 * (473 + a * 254)))
 
                 return {i: a, j: b, player: false};
 
         }
         //Posicão do Vértice do 1° quadrado externo
         //x = y = (346-254/2)*3/4=164.25
-        x = Math.trunc((x_ - 164.25) / 190.5);
-        y = Math.trunc((y_ - 164.25) / 190.5);
+        x = Math.trunc((x_ - 219*resolução/1200) / 190.5);
+        y = Math.trunc((y_ - 219*resolução/1200) / 190.5);
         return {i: x, j: y, player: true};
     }
 
@@ -188,9 +182,9 @@ class SideBar extends React.Component {
             for (let l = 0; l < 2; l++) {
                 if (k != 0 && l != 0)
                     if (matrizExterna) {
-                        if (validar(i - k, j - l, false))
+                        if (this.validar(i - k, j - l, false))
                             a.push({i: i - k, j: j - l, player: player});
-                    } else if (validar(i + k, j + l, true))
+                    } else if (this.validar(i + k, j + l, true))
                         a.push({i: i + k, j: j + l, player: player});
 
             }
@@ -262,7 +256,7 @@ class SideBar extends React.Component {
                 </Drawer>
                 <div className={"Board"}>
                     <header className="Board-header">
-                        <img className="Board-chess" src={board} onClick={e => this.handleClick(e)} alt={"tabuleiro"}/>
+                        <img className="Board-chessA" style={this.state.resolução} src={board} onClick={e => this.handleClick(e)} alt={"tabuleiro"}/>
                         <img className={"Hata"} src={[empty, hatIa, hat][this.state.matrizExterna[0][0].player]}
                              alt={"hat"}
                              style={this.state.matrizExterna[0][0].posição} onClick={this.setMatrizExterna}/>
